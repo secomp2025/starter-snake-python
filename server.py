a786dae59647887d325fd0a9e987b11328f636a5
secomp2025 <teles.v@aluno.ifsp.edu.br>
2025-06-03 21:47:33 -0300

    Update default server port

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def run_server(handlers: typing.Dict):
         return response
 
     host = "0.0.0.0"
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "5000"))
 
     logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
